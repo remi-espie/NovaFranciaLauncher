0e680282d7293fbf5f057c5b076ffd4b3c198102
TheFlash787 <sonically@hotmail.co.uk>
2020-02-09 22:33:35 +0000

    Disabled RSS comments being shown, and reworked formatting for announcements from Discord.

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -757,7 +757,7 @@ const newsContent                   = document.getElementById('newsContent')
 const newsArticleTitle              = document.getElementById('newsArticleTitle')
 const newsArticleDate               = document.getElementById('newsArticleDate')
 const newsArticleAuthor             = document.getElementById('newsArticleAuthor')
-const newsArticleComments           = document.getElementById('newsArticleComments')
+// const newsArticleComments           = document.getElementById('newsArticleComments')
 const newsNavigationStatus          = document.getElementById('newsNavigationStatus')
 const newsArticleContentScrollable  = document.getElementById('newsArticleContentScrollable')
 const nELoadSpan                    = document.getElementById('nELoadSpan')
@@ -1054,9 +1054,9 @@ function displayArticle(articleObject, index){
     newsArticleTitle.href = articleObject.link
     newsArticleAuthor.innerHTML = 'by ' + articleObject.author
     newsArticleDate.innerHTML = articleObject.date
-    newsArticleComments.innerHTML = articleObject.comments
-    newsArticleComments.href = articleObject.commentsLink
-    newsArticleContentScrollable.innerHTML = '<div id="newsArticleContentWrapper"><div class="newsArticleSpacerTop"></div>' + articleObject.content + '<div class="newsArticleSpacerBot"></div></div>'
+    // newsArticleComments.innerHTML = articleObject.comments
+    // newsArticleComments.href = articleObject.commentsLink
+    newsArticleContentScrollable.innerHTML = '<div id="newsArticleContentWrapper"><div class="newsArticleSpacerTop"></div>' + articleObject.content.replace(/<:br:676190676458668072>/g, '<br>').replace(/\*/g, '').replace(/_/g, '').replace(/`/g, '') + '<div class="newsArticleSpacerBot"></div></div>'
     Array.from(newsArticleContentScrollable.getElementsByClassName('bbCodeSpoilerButton')).forEach(v => {
         v.onclick = () => {
             const text = v.parentElement.getElementsByClassName('bbCodeSpoilerText')[0]
@@ -1091,11 +1091,11 @@ function loadNews(){
                         const date = new Date(el.find('pubDate').text()).toLocaleDateString('en-US', {month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'})
 
                         // Resolve comments.
-                        let comments = el.find('slash\\:comments').text() || '0'
-                        comments = comments + ' Comment' + (comments === '1' ? '' : 's')
+                        // let comments = el.find('slash\\:comments').text() || '0'
+                        // comments = comments + ' Comment' + (comments === '1' ? '' : 's')
 
                         // Fix relative links in content.
-                        let content = el.find('content\\:encoded').text()
+                        let content = el.find('description').text()
                         let regex = /src="(?!http:\/\/|https:\/\/)(.+?)"/g
                         let matches
                         while((matches = regex.exec(content))){
@@ -1114,8 +1114,8 @@ function loadNews(){
                                 date,
                                 author,
                                 content,
-                                comments,
-                                commentsLink: link + '#comments'
+                                // comments,
+                                // commentsLink: link + '#comments'
                             }
                         )
                     }
