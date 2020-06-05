a3115d645e8aa560fafd12e2d1b44006cd618f26
TheFlash787 <sonically@hotmail.co.uk>
2020-06-05 18:07:24 +0100

    Updated the installation folder name

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,4 +1,4 @@
-const APP_DATA_NAME = 'MRLauncherData'
+const APP_DATA_NAME = 'ModRealmsLauncher'
 const DISTRIBUTION_URL = 'https://files.modrealms.net/launcher/distribution.json'
 
 module.exports = { APP_DATA_NAME, DISTRIBUTION_URL }
