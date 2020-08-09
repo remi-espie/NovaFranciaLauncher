030e0591b23caf9930fd40195f06e0101781cbfc
TheFlash787 <sonically@hotmail.co.uk>
2020-08-09 13:12:46 +0100

    Bumped up the default launcher window size slightly for the larger server info size.

--- index.js
+++ index.js
@@ -107,8 +107,8 @@ let win
 function createWindow() {
 
     win = new BrowserWindow({
-        width: 980,
-        height: 552,
+        width: 1500,
+        height: 850,
         icon: getPlatformIcon('SealCircle'),
         frame: false,
         webPreferences: {
