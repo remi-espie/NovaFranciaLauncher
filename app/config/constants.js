e20b60bcb1f9d40140f78b1e5d11e62c644a2e3a
TheFlash787 <sonically@hotmail.co.uk>
2020-08-10 01:37:16 +0100

    Updated the distribution url to pull from GitLab instead of the webserver.

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,4 +1,4 @@
 const APP_DATA_NAME = 'ModRealmsLauncher'
-const DISTRIBUTION_URL = 'https://files.modrealms.net/launcher/distribution.json'
+const DISTRIBUTION_URL = 'https://gitlab.com/modrealms/launcher-packs/-/jobs/artifacts/master/raw/distribution.json?job=build'
 
 module.exports = { APP_DATA_NAME, DISTRIBUTION_URL }
