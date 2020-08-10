bfa53e0a5264df00e165f896088051d1ff920af4
TheFlash787 <sonically@hotmail.co.uk>
2020-08-10 05:58:49 +0100

    Changed ModRealmsLauncher to ModRealms Launcher

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,4 +1,4 @@
-const APP_DATA_NAME = 'ModRealmsLauncher'
+const APP_DATA_NAME = 'ModRealms Launcher'
 const DISTRIBUTION_URL = 'https://gitlab.com/modrealms/launcher-packs/-/jobs/artifacts/master/raw/distribution.json?job=build'
 
 module.exports = { APP_DATA_NAME, DISTRIBUTION_URL }
--- build.js
+++ build.js
@@ -19,7 +19,7 @@ builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
         appId: 'modrealmslauncher',
-        productName: 'ModRealmsLauncher',
+        productName: 'ModRealms Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
         copyright: 'Copyright © 2018-2020 Daniel Scalzi',
         directories: {
