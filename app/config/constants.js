b661a27696e5062d9ae1d8f3834160bbb103f23e
TheFlash787 <sonically@hotmail.co.uk>
2020-08-11 03:35:01 +0100

    Updated the distribution.json URL

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,4 +1,4 @@
 const APP_DATA_NAME = 'ModRealms Launcher'
-const DISTRIBUTION_URL = 'https://gitlab.com/modrealms/launcher-packs/-/jobs/artifacts/master/raw/distribution.json?job=build'
+const DISTRIBUTION_URL = 'https://launcher.modrealms.net/deploy/distribution.json'
 
 module.exports = { APP_DATA_NAME, DISTRIBUTION_URL }
