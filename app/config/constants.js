9bd352e6c25f982614a1b6c05166f6d53e04e359
TheFlash787 <sonically@hotmail.co.uk>
2020-08-13 06:29:58 +0100

    ModRealms Launcher => ModRealmsLauncher in the appdata folder.

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,4 +1,4 @@
-const APP_DATA_NAME = 'ModRealms Launcher'
+const APP_DATA_NAME = 'ModRealmsLauncher'
 const DISTRIBUTION_URL = 'https://launcher.modrealms.net/deploy/distribution.json'
 
 module.exports = { APP_DATA_NAME, DISTRIBUTION_URL }
