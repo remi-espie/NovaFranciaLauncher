8ecf44abe9ab7d199949935d95f39b50492943ae
TheFlash787 <sonically@hotmail.co.uk>
2021-05-05 01:23:26 +0100

    The landing avatar image now contains skin overlays.

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -203,7 +203,7 @@ function updateSelectedAccount(authUser){
             username = authUser.displayName
         }
         if(authUser.uuid != null){
-            document.getElementById('avatarContainer').style.backgroundImage = `url('https://crafatar.com/renders/body/${authUser.uuid}')`
+            document.getElementById('avatarContainer').style.backgroundImage = `url('https://crafatar.com/renders/body/${authUser.uuid}?default=MHF_Steve&overlay')`
         }
     }
     user_text.innerHTML = username
